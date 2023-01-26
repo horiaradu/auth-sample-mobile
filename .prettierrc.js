a33623ba0a36010f1c5109de55e45210d3232c4f
Horia Radu <horia.radu@e-spres-oh.com>
2023-01-26 12:13:39 +0200

    Prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 80,
+  arrowParens: 'always',
 };
