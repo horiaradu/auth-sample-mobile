ddcf20595bb0b085071ee8b9848b9f04900e13a5
Horia Radu <horia.radu@e-spres-oh.com>
2023-01-26 17:38:50 +0200

    Store token in secure storage

--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@
  * @format
  */
 
+import React from 'react';
 import axios from 'axios';
-import React, { useState } from 'react';
 import {
   Button,
   SafeAreaView,
@@ -16,26 +16,30 @@ import {
   View,
 } from 'react-native';
 
-import { useAuth0, Auth0Provider } from 'react-native-auth0';
-import { AUTH0_AUDIENCE, AUTH0_DOMAIN, AUTH0_CLIENT_ID } from '@env';
+import { useAuth0, Auth0Provider, Credentials } from 'react-native-auth0';
+import { AUTH0_AUDIENCE, AUTH0_DOMAIN, AUTH0_CLIENT_ID, API_URL } from '@env';
+
+import * as Keychain from 'react-native-keychain';
 
 import { Colors, Header } from 'react-native/Libraries/NewAppScreen';
 
 function LoginButton({
   onComplete,
 }: {
-  onComplete: (accessToken: string) => void;
+  onComplete: (credentials: Credentials) => void | Promise<void>;
 }) {
   const { authorize, getCredentials } = useAuth0();
 
   const onPress = async () => {
     try {
       await authorize({
-        scope: 'openid profile email',
+        scope: 'openid profile email offline_access',
         audience: AUTH0_AUDIENCE,
       });
       const credentials = await getCredentials();
-      onComplete(credentials.accessToken);
+
+      console.log(credentials);
+      await onComplete(credentials);
     } catch (e) {
       console.log(e);
     }
@@ -44,12 +48,17 @@ function LoginButton({
   return <Button onPress={onPress} title="Log in" />;
 }
 
-function LogoutButton() {
+function LogoutButton({
+  onComplete,
+}: {
+  onComplete: () => void | Promise<void>;
+}) {
   const { clearSession } = useAuth0();
 
   const onPress = async () => {
     try {
       await clearSession();
+      await onComplete();
     } catch (e) {
       console.log(e);
     }
@@ -76,8 +85,6 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const [accessToken, setAccessToken] = useState<string | null>(null);
-
   return (
     <Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENT_ID}>
       <SafeAreaView style={backgroundStyle}>
@@ -91,13 +98,43 @@ function App(): JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Profile />
-          <LoginButton onComplete={(token) => setAccessToken(token)} />
-          <LogoutButton />
+          <LoginButton
+            onComplete={async (credentials) => {
+              await Keychain.setGenericPassword(
+                'authsample-username',
+                JSON.stringify(credentials),
+                {
+                  accessControl: Keychain.ACCESS_CONTROL.USER_PRESENCE,
+                  accessible:
+                    Keychain.ACCESSIBLE.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+                  authenticationType:
+                    Keychain.AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
+                },
+              );
+            }}
+          />
+          <LogoutButton
+            onComplete={async () => {
+              await Keychain.resetGenericPassword();
+            }}
+          />
 
           <Button
             onPress={async () => {
-              console.log('accessToken', accessToken);
-              axios.get('http://localhost:3000', {
+              const result = await Keychain.getGenericPassword({
+                accessControl: Keychain.ACCESS_CONTROL.USER_PRESENCE,
+                accessible: Keychain.ACCESSIBLE.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+                authenticationType:
+                  Keychain.AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
+              });
+              if (!result) {
+                console.log('No credentials stored');
+                return;
+              }
+
+              const accessToken = JSON.parse(result.password).accessToken;
+
+              axios.get(API_URL, {
                 headers: { Authorization: `Bearer ${accessToken}` },
               });
             }}
--- types/env.d.ts
+++ types/env.d.ts
@@ -2,4 +2,5 @@ declare module '@env' {
   export const AUTH0_AUDIENCE: string;
   export const AUTH0_DOMAIN: string;
   export const AUTH0_CLIENT_ID: string;
+  export const API_URL: string;
 }
